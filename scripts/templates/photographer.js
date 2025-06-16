cc1dd93e639c196cb62bc7b58e6ca450c4bd92eb
amandinedev <amandinehazard@live.fr>
2025-06-16 08:36:33 +0200

    feature-import-photographers-data

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,6 +1,6 @@
 function photographerTemplate(data) {
     //extract photographer data properties
-    const { name, portrait } = data;
+    const { name, id, city, country, tagline, price, portrait} = data;
 
     //construct a full URL access to the portrait image
     const picture = `assets/photographers/${portrait}`;
@@ -10,11 +10,35 @@ function photographerTemplate(data) {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        img.setAttribute("alt", "${name}")
+        img.className = "Article__portrait";
         const h2 = document.createElement( 'h2' );
+        h2.className = "Article__title";
         h2.textContent = name;
+        // Create info container with additional details about the photographer.
+        articleInfo = document.createElement( 'div' );
+        articleInfo.className = "Article__info";
+        // Location and country
+        const pLocation = document.createElement( 'p' );
+        pLocation.className = "Article__info--location";
+        pLocation.textContent = city+ ", " + country;
+        // Tagline
+        const pTagline = document.createElement('p');
+        pTagline.className = "Article__info--tagline";
+        pTagline.textContent = tagline;
+        // Price
+        const pPrice = document.createElement( 'p' );
+        pPrice.className = "Article__info--price";
+        pPrice.textContent = price + "€/jour";    
+        // Append all elements
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(articleInfo);
+        articleInfo.appendChild(pLocation);
+        articleInfo.appendChild(pTagline);
+        articleInfo.appendChild(pPrice);
+        
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, id, city, country, tagline, price, picture, getUserCardDOM }
 }
