c0118cfc0457b518feee64973ec97da41bcc3a3d
amandinedev <amandinehazard@live.fr>
2025-07-19 11:45:19 +0200

    fix meta description page photographer

--- scripts/templates/photographer-modal.js
+++ scripts/templates/photographer-modal.js
@@ -49,7 +49,7 @@ function modalTemplate(info) {
           type="text"
           id="message"
           name="message"
-          aria-label= votre-message
+          aria-label="votre message"
         ></textarea>
       </div>
         <button type="submit" id=submit-button class="brown-button submit" aria-label="Envoyer le formulaire de contact">Envoyer</button>
